c56d5ee4033069fe5f2aa7dd2a6db974a3226914
christian torres <christian96torres@yahoo.com>
2023-02-20 13:59:31 -0700

    Added pracitce JavaScript and messing around with css and html

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
-//alert("I am connected");
+const greeting = () => {
+let userName = prompt("What is your name");
+
+document.querySelector("#devName").innerHTML = `Welcome, ${userName}`
+
+};
+
+greeting();
